a768f3a613eece9efbb5f173ee18acab37790ca6
davidly <davidly_lee@hotmail.com>
2026-01-20 13:20:05 -0800

    make e run twice as fast

--- 6502/e.s
+++ 6502/e.s
@@ -24,34 +24,29 @@
 ;      return 0;
 ;    }
 ;
-; y is N
-; x is n
 
     .cr      6502
     .tf      e.hex, AP1, 8
-    .or      $1000
+    .or      $200
 
 echo         .eq     $ffef          ; sends the character in a to the console
 prbyte       .eq     $ffdc          ; sends the value in a to the console in hex
 exitapp      .eq     $ff1f          ; ends the app / returns to the Apple 1 monitor
 
-array        .eq     $2000          ; starting at offset 8192.
-array_digits .eq     200            ; each digit is just a byte
-
 ; The Apple 1 reserves locations 0x0024 through 0x002b in the 0 page for the monitor.
 
-var_curarray     .eq $30            ; 2 bytes
-var_bigN         .eq $32            ; 1 byte
-var_n            .eq $33            ; 1 byte
-var_x            .eq $34            ; 2 bytes
-var_tmp          .eq $36            ; 1 byte
-var_tmp2         .eq $37            ; 1 byte
-var_opA          .eq $38            ; 2 bytes
-var_opB          .eq $3a            ; 2 bytes
-var_divRem       .eq $3c            ; 2 bytes
-var_tmpWord      .eq $3e            ; 2 bytes
-var_printString  .eq $40            ; 2 bytes
-var_arrayOffset  .eq $42            ; 2 bytes
+var_bigN         .eq $10            ; 1 byte
+var_n            .eq $11            ; 1 byte
+var_x            .eq $12            ; 2 bytes
+var_tmp          .eq $14            ; 2 bytes
+var_opA          .eq $16            ; 2 bytes
+var_opB          .eq $18            ; 2 bytes
+var_divRem       .eq $1a            ; 2 bytes
+var_printString  .eq $1c            ; 2 bytes
+var_arrayOffset  .eq $1e            ; 2 bytes
+
+array            .eq $38            ; starting at offset 0x38 through 0xff
+array_digits     .eq 200            ; each digit is just a byte
 
     jmp      start
 
@@ -65,40 +60,25 @@ start
     lda      #0
     sta      var_x
     sta      var_x+1
-    
+
     ; for (n = N - 1; n > 0; --n)
     ;     a[n] = 1;
-    lda      #array
-    sta      var_curarray
-    lda      /array
-    sta      var_curarray+1
-    ldy      #0
-    ldx      #0
+    ldx      #array_digits-1
 
 _initialize_next:
     lda      #1
-    sta      (var_curarray),y
-    inc      var_curarray
-    inx
-    cpx      #array_digits
+    sta      array,x
+    dex
     bne      _initialize_next
 
     ; a[1] = 2, a[0] = 0;
-    lda      #array
-    sta      var_curarray
-    lda      /array
-    sta      var_curarray+1
     lda      #0
-    sta      (var_curarray),y
-    inc      var_curarray
+    sta      array,x
     lda      #2
-    sta      (var_curarray),y
+    sta      array+1,x
 
     lda      #array_digits          ; N = digits
     sta      var_bigN
-
-    lda      /array                                             ; array is on a 256-byte page boundry and will never wrap
-    sta      var_curarray+1
 
 _outer:
     lda      #9                     ; while (N > 9)
@@ -107,20 +87,13 @@ _outer:
     dec      var_bigN               ; N = N - 1
     lda      var_bigN               ; n = N
     sta      var_n
+    tay                             ; put n in Y
 
-    lda      #array                 ; make var_currarry & array[ n ]
-    sta      var_curarray
-    clc
-    lda      var_n                  ; add n to var_curarray. this won't wrap a page boundry since n < 256
-    adc      var_curarray
-    sta      var_curarray
-                  
 _inner
     jsr      div_mod_x_by_n         ; remainder in A and division result in X
-    ldy      #0                     ; print_uint and div_mod_x_by_n can trash y, so reset it to 0
-    sta      (var_curarray),y
-    dec      var_curarray           ; --n for curarray
-    lda      (var_curarray),y       ; a[ n - 1 ]
+    ldy      var_n                  ; y may have been trashed in div_mod; restore it
+    sta      array,y
+    lda      array-1,y              ; a[ n - 1 ]
     jsr      mul_a_by_10            ; x = A * 10 ... this is a two-byte quantity
     txa
     clc
@@ -148,62 +121,74 @@ _all_done:
 ; multiply value in register A by 10
 ; stores 2-byte result in var_x
 mul_a_by_10:
-    sta      var_tmp
-    lda      #0
-    sta      var_x
-    sta      var_x+1
-    lda      #10
-    sta      var_tmp2
-_mul_10_next:
-    lda      var_x
-    clc
-    adc      var_tmp
-    sta      var_x
-    lda      var_x+1
-    adc      #0
-    sta      var_x+1
-    dec      var_tmp2
-    bne      _mul_10_next
+    sta      var_x                  ; store input in x
+    lda      #$00
+    sta      var_x+1                ; clear result's high byte
+    lda      var_x                  ; restore x into a
+    asl      a                      ; a = 2x
+    rol      var_x+1                ; carry into high byte
+    asl      a                      ; a = 4x
+    rol      var_x+1                ; carry into high byte
+    clc                             ; clear carry for addition
+    adc      var_x                  ; a = 4x + x = 5x
+    sta      var_x                  ; store 5x low byte
+    lda      var_x+1                ; load high byte
+    adc      #0                     ; add carry from (4x + x)
+    sta      var_x+1                ; store 5x high byte
+    asl      var_x                  ; low byte (5x * 2) = 10x
+    rol      var_x+1                ; final shift into high byte for 10x
     rts
 
-; divide unsigned 2-byte number in var_x by unsigned 1-byte value n.
-; store remainder in register a and quotient in register x. 
+; Input:  Dividend in two bytes at var_x
+;         Divisor in one byte at var_n
+; Output: Quotient in Register X
+;         Remainder in Register A
 div_mod_x_by_n:
-    lda      var_x
-    sta      var_opB
+    lda      var_x                  ; copy var_x so it isn't modified
+    sta      var_tmp
     lda      var_x+1
-    sta      var_opB+1
-    lda      var_n
-    sta      var_opA
-    lda      #0
-    sta      var_opA+1
-    jsr      uidiv
-    ldx      var_opB
-    lda      var_divRem
-    rts
+    sta      var_tmp+1
+    lda      #0                     ; initialize remainder (a) to 0
+    ldx      #16                    ; loop for each of the 16 bits in the dividend
+
+div_loop:
+    asl      var_tmp                ; shift dividend low byte left
+    rol      var_tmp+1              ; shift dividend high byte left, carry into remainder
+    rol      a                      ; rotate dividend's high bit into remainder (a)
+    cmp      var_n                  ; compare remainder (a) with divisor in var_n
+    bcc      skip_sub               ; if remainder < divisor, skip subtraction
+    sbc      var_n                  ; remainder = remainder - divisor
+    inc      var_tmp                ; set the lowest bit of the quotient
+
+skip_sub:
+    dex                             ; decrement bit counter
+    bne      div_loop               ; repeat for all 16 bits
+
+    ldx      var_tmp                ; move quotient (low byte of result) to x
+    rts                             ; return with remainder in a, quotient in x
 
 ; divide unsigned 2-byte number in opB by unsigned 2-byte number in opA.
-; result is in opB. remainder is in var_divREM. 
+; result is in opB. remainder is in var_divREM.
 uidiv:
-    lda      #0               ; Initialize var_divREM to 0
+    lda      #0                     ; Initialize var_divREM to 0
     sta      var_divREM
     sta      var_divREM+1
-    ldx      #16              ; There are 16 bits in var_opB
+    ldx      #16                    ; There are 16 bits in var_opB
 _uidiv_l1:
-    asl      var_opB          ; Shift hi bit of var_opB into var_divREM
-    rol      var_opB+1        ; (vacating the lo bit, which will be used for the quotient)
+    asl      var_opB                ; Shift hi bit of var_opB into var_divREM
+    rol      var_opB+1              ; (vacating the lo bit, which will be used for the quotient)
     rol      var_divREM
     rol      var_divREM+1
     lda      var_divREM
-    sec                       ; Trial subtraction
+    sec                             ; Trial subtraction
     sbc      var_opA
     tay
     lda      var_divREM+1
     sbc      var_opA+1
-    bcc      _uidiv_l2        ; Did subtraction succeed?
-    sta      var_divREM+1     ; If yes, save it
+    bcc      _uidiv_l2              ; Did subtraction succeed?
+    sta      var_divREM+1           ; If yes, save it
     sty      var_divREM
-    inc      var_opB          ; and record a 1 in the quotient
+    inc      var_opB                ; and record a 1 in the quotient
 _uidiv_l2:
     dex
     bne      _uidiv_l1
@@ -223,9 +208,9 @@ _prstr_done:
 
 print_uint:
     lda      var_opA
-    sta      var_tmpWord
+    sta      var_tmp
     lda      var_opA+1
-    sta      var_tmpWord+1
+    sta      var_tmp+1
     lda      #intString
     clc
     adc      #5
@@ -236,9 +221,9 @@ print_uint:
     inc      var_arrayOffset+1
 _print_no_carry:
 _print_uint_again:
-    lda      var_tmpWord
+    lda      var_tmp
     sta      var_opB
-    lda      var_tmpWord+1
+    lda      var_tmp+1
     sta      var_opB+1
     lda      #10
     sta      var_opA
@@ -256,9 +241,9 @@ _print_no_hidec:
     ldy      #0
     sta      (var_arrayOffset), y
     lda      var_opB+1
-    sta      var_tmpWord+1
+    sta      var_tmp+1
     lda      var_opB
-    sta      var_tmpWord
+    sta      var_tmp
     bne      _print_uint_again
     cmp      var_opB+1
     bne      _print_uint_again
